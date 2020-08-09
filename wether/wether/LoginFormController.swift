f95d47e4996040fde902362aeefe867bbe64bacd
irocool <ikuligina@gmail.com>
2020-08-09 20:46:52 +0300

    create LoginFormController

---
+++ wether/wether/LoginFormController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginFormController.swift
+//  wether
+//
+//  Created by Catalina on 03/08/2020.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var LoginText: UITextField!
+    
+    @IBOutlet weak var PassText: UITextField!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
